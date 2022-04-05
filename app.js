da3b42bbf95890ec87966ec8181f5b2bcfa65430
KmichaelT <kmichaeltb@gmail.com>
2022-04-05 03:18:42 -0400

    improved

--- app.js
+++ app.js
@@ -12,15 +12,26 @@ function onclick(e){
         label=document.getElementById('lbl');
         counter.textContent=`${day}`
         e.target.textContent='200'
+        
             
         }
         else{
-        e.target.parentElement.setAttribute("class", "sqr-done position-relative text-center")
+        
+            e.target.parentElement.setAttribute("class", "sqr-done position-relative text-center")
         day++;
         counter=document.getElementById('number');
         label=document.getElementById('lbl');
         counter.textContent=`${day}`
-        e.target.textContent='done' 
+        e.target.textContent='done'
+
+        Swal.fire({
+            icon: 'success',
+            title: 'Good Job',
+            text: 'Keep The Momentum!',
+            confirmButtonColor: '#00a724',
+            confirmButtonText: '👍'
+          })
+        
         }
 
     }
